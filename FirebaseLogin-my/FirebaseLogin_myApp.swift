d0d13673d0a5bc8081f12eb683c5b13e1d987243
myk-sh <mikhail_sh@icloud.com>
2023-08-02 10:43:14 +0700

    Initial Commit

---
+++ FirebaseLogin-my/FirebaseLogin_myApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseLogin_myApp.swift
+//  FirebaseLogin-my
+//
+//  Created by Misha Shapkin on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseLogin_myApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
